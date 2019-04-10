69f6c27b7dc0d2512023fed1506166045228b41d
Трушин Павел <ptrushin@custis.ru>
2019-04-10 08:51:00 +0000

    Initial

---
+++ PostAction.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.OData.Query;
+
+namespace OData.PostAction
+{
+    public static class PostAction
+    {
+        public static IEnumerable<T> PostActionOnQuery<T>(
+            IQueryable<T> query, 
+            ODataQueryOptions<T> options, 
+            Action<ICollection<T>> postAction)
+            where T : class, new()
+        {
+            var queryable = options.ApplyTo(query);
+            var itemType = queryable.GetType().GetGenericArguments().First();
+            var list = ToTypedList(queryable, itemType);
+            var entityList = GetEntityList<T>(list).ToList();
+            postAction(entityList);
+            return entityList;
+        }
+
+        private static IList ToTypedList(IQueryable self, Type innerType)
+        {
+            var methodInfo = typeof(Enumerable).GetMethod(nameof(Enumerable.ToList));
+            var genericMethod = methodInfo.MakeGenericMethod(innerType);
+            return genericMethod.Invoke(null, new object[]
+            {
+                self
+            }) as IList;
+        }
+
+        private static IEnumerable<T> GetEntityList<T>(IEnumerable enumerable)
+            where T : class, new()
+        {
+            var type = typeof(T);
+            foreach (var item in enumerable)
+            {
+                yield return (T)GetValue(type, item);
+            }
+        }
+
+        private static object GetValue(Type type, object item)
+        {
+            if (item == null)
+            {
+                return null;
+            }
+            else if (type.IsAssignableFrom(item.GetType()))
+            {
+                return item;
+            }
+            else if (item.GetType().Name == "SelectAllAndExpand`1") //$expand
+            {
+                var entityProperty = item.GetType().GetProperty("Instance");
+                return entityProperty.GetValue(item);
+            }
+            else if (item.GetType().Name == "SelectSome`1") //$select
+            {
+                var dict = ((ISelectExpandWrapper)item).ToDictionary();
+                return ToObject(type, dict);
+            }
+            else 
+            {
+                return null;
+            }
+        }
+
+        private static object ToObject(Type type, IDictionary<string, object> source)
+        {
+            var newObject = Activator.CreateInstance(type);
+
+            foreach (var item in source)
+            {
+                var property = type.GetProperty(item.Key);
+                var propertyType = property.PropertyType;
+                property.SetValue(newObject, GetValue(propertyType, item.Value), null);
+            }
+
+            return newObject;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# OData.PostAction
+OData.PostAction
+
+Add post action to odata controller action.
+
+# Using 
+
+```
+/// <summary>User</summary>
+...
+public IActionResult GetMyEntity(ODataQueryOptions<PartialPlan> options)
+{
+    var query = DbContext.MyEntity;;
+
+    Action<ICollection<MyEntity>> postAction = collection => {
+        var keys = collection.Select(e => e.RemoteEntityId).Distinct().ToArray();
+        var remoteEntityList = RemoteApi.GetRemoteEntityList(keys).ToDictionary(e => e.Key);
+        collection.ForEach(e => e.RemoteEntity = e.RemoteEntityId == null ? null : remoteEntityList.GetValueOrDefault(e.RemoteEntityId));
+    };
+    
+    return PostAction.PostActionOnQuery(query, options, postAction);
+}
+...
+```
+
+# Links
+* Nuget - https://www.nuget.org/packages/EFCore.DbComment/
+
+# Thanks
+* https://github.com/gorillapower for https://github.com/OData/WebApi/issues/521#issuecomment-281309496
+* https://stackoverflow.com/users/3191951/kedrzu for https://stackoverflow.com/questions/47094145/how-to-fill-some-properties-in-model-after-evaluation-of-iqueryable-in-odata/47113000#47113000
